352fe432501ab1fd91b805b688a258e19bbee89f
Mark Lopez <m@silvenga.com>
2016-09-25 22:51:00 -0500

    Added XMLDocs to IJobActivatorFilter.

--- src/Hangfire.ActivationExtensions/Interceptor/IJobActivatorFilter.cs
+++ src/Hangfire.ActivationExtensions/Interceptor/IJobActivatorFilter.cs
@@ -4,12 +4,30 @@
 
     public interface IJobActivatorFilter
     {
+        /// <summary>
+        /// Called before the job is constructed by the current activator.
+        /// </summary>
+        /// <param name="jobType">The type of the job being created.</param>
         void OnMaterializing(Type jobType);
 
+        /// <summary>
+        /// Called after the job is constructed by the current activator.
+        /// </summary>
+        /// <param name="jobType">The type of the job being created.</param>
+        /// <param name="activatedJob">The object created by the activator. Should be of type found within jobType.</param>
         void OnMaterialized(Type jobType, object activatedJob);
 
+        /// <summary>
+        /// Called before the scope is constructed by the current activator.
+        /// </summary>
+        /// <param name="context">Context of the activator.</param>
         void OnScopeCreating(JobActivatorContext context);
 
+        /// <summary>
+        /// Called after the scope is constructed by the current activator.
+        /// </summary>
+        /// <param name="context">Context of the activator.</param>
+        /// <param name="createdScope">The scope created by the activator.</param>
         JobActivatorScope OnScopeCreated(JobActivatorContext context, JobActivatorScope createdScope);
     }
 }
